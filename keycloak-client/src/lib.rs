5d8e1b075f20e61d11be242d40c5379a1391bc91
Enno Boland <g@s01.de>
2025-03-01 13:24:47 +0100

    re-expost common oauth2 types

--- keycloak-client/src/lib.rs
+++ keycloak-client/src/lib.rs
@@ -6,6 +6,10 @@ pub use client::*;
 pub use error::*;
 pub use response::*;
 
+pub use oauth2::{
+    AccessToken, AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken,
+    RedirectUrl, RefreshToken, TokenResponse, TokenUrl,
+};
 pub use reqwest::{
     Client, ClientBuilder, Method, StatusCode, Url, Version, header,
 };
